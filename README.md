02639621955c2c00411410d51ed26663c4bc3f97
jakobPeer <peernetworkpse@gmail.com>
2025-03-11 16:27:45 +0100

    fix(init): uploaded the wrong version initialy. Re-Uploading the right one

--- README.md
+++
@@ -1,3 +0,0 @@
-# peer_website
-This is the repository for the Website code.  
-[![Contributing](https://img.shields.io/badge/Contributing-Guidelines-blue.svg)](https://github.com/peer-network/.github/blob/main/CONTRIBUTING.md)  
--- scripts/carouselscript.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const carousel = document.getElementById("carousel");
-    const clone = carousel.innerHTML;
-    carousel.innerHTML += clone; // Duplicate content for infinite effect
-});
